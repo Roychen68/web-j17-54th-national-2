22da1bc9cdd2e3eb693d157a03039299e2de5467
3.57 <aeustusr3.57@gmail.com>
2025-04-27 13:59:35 +0800

    Add files via upload

---
+++ 01_02/api/del.php
@@ -0,0 +1,27 @@
+<?php
+include "db.php";
+
+$action = $_POST['action'];
+switch ($action) {
+    case 'bus':
+        $pdo->query("DELETE FROM `bus` WHERE `id` = '{$_POST['id']}'");
+        break;
+    
+    case 'station':
+        $pdo->query("DELETE FROM `station` WHERE `id` = '{$_POST['id']}'");
+        break;
+
+    case 'participants':
+        $pdo->query("DELETE FROM `participants` WHERE `id` = '{$_POST['id']}'");
+        break;
+    
+    case 'response':
+        $pdo->query("DELETE FROM `response` WHERE `id` = '{$_POST['id']}'");
+        break;
+    case 'route':
+        $pdo->query("DELETE FROM `route` WHERE `id` = '{$_POST['id']}'");
+        $pdo->query("DELETE FROM `bus` WHERE `route` = '{$_POST['route']}'");
+        $pdo->query("DELETE FROM `station` WHERE `route` = '{$_POST['route']}'");
+        break;
+}
+?>
