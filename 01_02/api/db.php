cbb50a9ebc9a1653d4445b196fa6d8b612cc76f6
3.57 <aeustusr3.57@gmail.com>
2025-05-06 22:49:26 +0800

    Delete 01_02 directory

--- 01_02/api/db.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-$pdo = new PDO ("mysql:host=localhost;charset=utf8;dbname=source01","admin","1234");
-?>
--- 01_02/api/login.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "db.php";
-$login = $pdo->query("SELECT * FROM `admin` WHERE `username` = '{$_POST['username']}' AND `password` = '{$_POST['password']}'")->fetchColumn();
-echo $login;
-if ($login > 0) {
-    $_SESSION['admin'] = true;
-}
